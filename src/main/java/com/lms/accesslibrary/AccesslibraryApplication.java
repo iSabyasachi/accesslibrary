66428f5da2418e19d5b10fbaff70cace4b8c9c0b
iSabyasachi <s.sabyasachi@outlook.com>
2022-05-12 23:46:21 -0400

    first version

---
+++ src/main/java/com/lms/accesslibrary/AccesslibraryApplication.java
@@ -0,0 +1,13 @@
+package com.lms.accesslibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesslibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesslibraryApplication.class, args);
+	}
+
+}
